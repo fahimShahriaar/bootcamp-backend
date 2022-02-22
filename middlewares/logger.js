50c4f2e7c1185bc4ef190ef566a2ce0601af07d3
Shahriar <fem.nwu@gmail.com>
2022-02-22 20:09:58 +0600

    updated

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,5 +1,8 @@
 const logger = (req, res, next) => {
-    console.log(`${req.mehtod} ${req.protocol}://${req.host}:${req.originalUrl}`);
-}
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+  next();
+};
 
 module.exports = logger;
--- server.js
+++ server.js
@@ -1,12 +1,40 @@
-const express = require('express');
-require('dotenv').config();
-const bootcamp = require('./routes/bootcamp');
+const express = require("express");
+require("dotenv").config();
+const bootcamp = require("./routes/bootcamp");
+const logger = require("./middlewares/logger");
+const mongoose = require("mongoose");
+const colors = require("colors");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// database connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((conn) =>
+    console.log(
+      `DB connected. name: ${conn.connection.name} port: ${conn.connection.port} host: ${conn.connection.host}`
+        .green.underline
+    )
+  )
+  .catch((err) => console.log(`${err}`.red.underline.bold));
+// (async function () {
+//   const conn = await mongoose.connect(process.env.MONGO_URI);
+//   const { host, port, name } = conn.connection;
+//   console.log(host);
+//   console.log(port);
+//   console.log(name);
+// })();
 
-app.use('/api/v1/bootcamps', bootcamp);
+// Dev logging middleware
+app.use(logger);
 
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamp);
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} on port: ${PORT}`));
+app.listen(PORT, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port: ${PORT}`.green.underline
+      .bold
+  )
+);
